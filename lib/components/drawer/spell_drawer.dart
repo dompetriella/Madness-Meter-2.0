15419c2a0e5f6ad9fa66107d07195e3a37bbfa75
Dominic Petriella <dpetriella@magicwrighter.com>
2023-10-09 16:54:06 -0400

    need to get spells working but otherwise done

--- lib/components/drawer/spell_drawer.dart
+++ lib/components/drawer/spell_drawer.dart
@@ -1,22 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:madness_meter_2/content/spells.dart';
-
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:madness_meter_2/db_functions.dart';
+import 'package:madness_meter_2/provider.dart';
 import '../../home_page.dart';
+import '../../models/spell.dart';
 import 'spell_description.dart';
 
-class SpellsDrawer extends StatelessWidget {
+late Future<List<Spell>> dbSpells;
+
+class SpellsDrawer extends HookConsumerWidget {
   const SpellsDrawer({
     super.key,
   });
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    useEffect(() {
+      try {
+        getAvailableSpellsInState(ref);
+      } catch (e) {
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text('Error: ' + e.toString())));
+      }
+      return null;
+    }, const []);
     return Drawer(
       width: MediaQuery.of(context).size.width - sideBarSize - 40,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(0),
       ),
-      backgroundColor: Colors.white.withOpacity(.15),
+      backgroundColor: Colors.white.withOpacity(.05),
       child: Row(
         children: [
           Flexible(
@@ -44,12 +58,13 @@ class SpellsDrawer extends StatelessWidget {
                     ),
                   ),
                   Flexible(
-                    child: ListView(
-                      children: [
-                        for (var i = 0; i < fakeSpellsList.length; i++)
-                          SpellDescription(spell: fakeSpellsList[i])
-                      ],
-                    ),
+                    child: ListView(children: [
+                      for (var i = 0;
+                          i < ref.watch(availableSpellsProvider).length;
+                          i++)
+                        SpellDescription(
+                            spell: ref.watch(availableSpellsProvider)[i])
+                    ]),
                   ),
                 ],
               ),
