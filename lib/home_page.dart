7e9b9dc5914891c0a604d52692970c8adf1079df
Dominic Petriella <dpetriella@magicwrighter.com>
2023-11-08 12:19:24 -0500

    created starting animation

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
@@ -93,6 +94,26 @@ class MadnessPage extends HookConsumerWidget {
   }
 }
 
+List<CongregateContainer> createCongregates(
+    AnimationController animationController) {
+  Random random = Random();
+  List<CongregateContainer> congregates = [];
+  int numberOfCongregates = 5 + random.nextInt(20 - 5);
+  for (var i = 0; i < numberOfCongregates; i++) {
+    double offsetX = -1000 + random.nextInt(2001).toDouble();
+    double offsetY = -1000 + random.nextInt(2001).toDouble();
+    double startingHeight = 50 + random.nextInt(251).toDouble();
+    double startingWidth = 50 + random.nextInt(751).toDouble();
+    congregates.add(CongregateContainer(
+        offsetX: offsetX,
+        offsetY: offsetY,
+        startingWidth: startingWidth,
+        startingHeight: startingHeight,
+        animationController: animationController));
+  }
+  return congregates;
+}
+
 double loginWidth = 800;
 double loginHeight = 300;
 
@@ -104,59 +125,88 @@ class Login extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     ValueNotifier<bool> isHeld = useState(false);
+    ValueNotifier<bool> isHeldLongEnough = useState(false);
+    var animationController =
+        useAnimationController(duration: 2000.ms, reverseDuration: 500.ms);
+    Random random = Random();
+
+    final animation =
+        CurvedAnimation(parent: animationController, curve: Curves.easeOutExpo);
+
+    Timer? _timer;
+
+    void _startTimer() {
+      _timer = Timer(Duration(seconds: 2), () {
+        if (isHeld.value) {
+          isHeldLongEnough.value = true;
+        }
+      });
+    }
+
+    void _cancelTimer() {
+      if (_timer != null && _timer!.isActive) {
+        _timer!.cancel();
+      }
+    }
 
     return Center(
         child: Stack(
       children: [
-        Stack(
-          clipBehavior: Clip.none,
-          children: [
-            // Positioned(
-            //   left: 0,
-            //   child: Container(
-            //     height: loginHeight,
-            //     width: loginWidth,
-            //     color: Colors.white,
-            //   ),
-            // )
-          ],
-        ),
+        AnimatedBuilder(
+            animation: animation,
+            builder: (context, child) {
+              return Stack(
+                  clipBehavior: Clip.none,
+                  children: createCongregates(animationController));
+            }),
         Center(
-          child: !isHeld.value
-              ? ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      padding: EdgeInsets.all(0),
-                      fixedSize: Size(loginWidth, loginHeight),
-                      shape: RoundedRectangleBorder(
-                          side: BorderSide(width: 2, color: Colors.white),
-                          borderRadius: BorderRadius.circular(20)),
-                      backgroundColor: Colors.transparent),
-                  onPressed: () {},
-                  onLongPress: () {
-                    Timer(Duration(milliseconds: (2000 > 500) ? 2000 - 500 : 0),
-                        () {
-                      isHeld.value = !isHeld.value;
-                    });
+          child: !isHeldLongEnough.value
+              ? GestureDetector(
+                  onPanDown: (details) async {
+                    isHeld.value = true;
+                    _startTimer();
+                    animationController.forward();
                   },
-                  child: Text('Hold',
-                      textAlign: TextAlign.center,
-                      style: GoogleFonts.astloch(
-                          fontSize: 90, color: Colors.white)),
+                  onTapUp: (details) {
+                    isHeld.value = false;
+                    _cancelTimer();
+                    animationController.reverse();
+                  },
+                  child: Container(
+                    height: loginHeight,
+                    width: loginWidth,
+                    decoration: BoxDecoration(
+                        color: Colors.transparent,
+                        borderRadius: BorderRadius.circular(20),
+                        border:
+                            Border.all(color: Colors.grey.shade200, width: 7)),
+                    child: Center(
+                      child: Text('Hold',
+                          textAlign: TextAlign.center,
+                          style: GoogleFonts.astloch(
+                              fontSize: 90, color: Colors.white)),
+                    ),
+                  ),
                 )
               : Center(
                   child: Container(
                       height: loginHeight,
                       width: loginWidth,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20)),
+                      decoration: BoxDecoration(boxShadow: [
+                        BoxShadow(
+                            color: Colors.white
+                                .withOpacity(animationController.value),
+                            spreadRadius: 50,
+                            blurRadius: 50)
+                      ], borderRadius: BorderRadius.circular(20)),
                       child: Center(
                           child: TextField(
                         inputFormatters: [UpperCaseTextFormatter()],
                         autofocus: true,
-                        cursorColor: Colors.red.shade900.withOpacity(.50),
+                        cursorColor: Colors.black,
                         style: TextStyle(
                             fontSize: 150,
-                            color: Colors.red.shade900.withOpacity(.50),
+                            color: Colors.black,
                             fontWeight: FontWeight.w900,
                             letterSpacing: 30),
                         decoration: InputDecoration(border: InputBorder.none),
@@ -174,8 +224,69 @@ class Login extends HookConsumerWidget {
                       ))),
                 ),
         ),
+        if (isHeldLongEnough.value)
+          Padding(
+            padding: const EdgeInsets.only(top: 24.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'ENTER',
+                  style: TextStyle(color: Colors.white, fontSize: 60),
+                ),
+                SizedBox(width: 30),
+                Text(
+                  'YOUR',
+                  style: TextStyle(color: Colors.white, fontSize: 60),
+                ),
+                SizedBox(
+                  width: 30,
+                ),
+                Text(
+                  'FATE',
+                  style: TextStyle(color: Colors.red, fontSize: 60),
+                ),
+              ].animate(interval: 800.ms).fadeIn().slideY(),
+            ),
+          )
       ],
     ));
+  }
+}
+
+class CongregateContainer extends StatelessWidget {
+  final AnimationController animationController;
+  final double offsetX;
+  final double offsetY;
+  final double startingWidth;
+  final double startingHeight;
+  const CongregateContainer({
+    super.key,
+    required this.offsetX,
+    required this.offsetY,
+    required this.startingWidth,
+    required this.startingHeight,
+    required this.animationController,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Transform.translate(
+      offset: Offset(offsetX - (offsetX * animationController.value),
+          offsetY - (offsetY * animationController.value)),
+      child: Center(
+        child: Container(
+          height: startingHeight +
+              ((loginHeight - startingHeight) * animationController.value),
+          width: startingWidth +
+              ((loginWidth - startingWidth) * animationController.value),
+          decoration: BoxDecoration(
+              borderRadius:
+                  BorderRadius.circular(20 * animationController.value),
+              color: Colors.white.withOpacity(animationController.value / 5)),
+        ),
+      ),
+    );
   }
 }
 
